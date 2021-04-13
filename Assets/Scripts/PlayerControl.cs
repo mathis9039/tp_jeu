10986abdb0d869074426f1ccb68d8f8003d1aa65
JayMunk <jaymunger10@live.ca>
2021-04-13 11:31:31 -0400

    premiere partie du Level
    Ajouts des gameObject,
    Modification des prefabs

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class move : MonoBehaviour
+public class PlayerControl : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
