310f72cbeac69b07a51368503f9a368b27357434
JayMunk <jaymunger10@live.ca>
2021-04-29 15:24:46 -0400

    Play video on crash

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class PlayerControl : MonoBehaviour
 {
@@ -11,9 +9,42 @@ public class PlayerControl : MonoBehaviour
     private AudioSource _audio;
     [SerializeField] private AudioClip carCrash;
     private bool isCrashed;
+    private VideoPlayer videoPlayer;
 
     public void Start()
     {
+        // Will attach a VideoPlayer to the main camera.
+        GameObject camera = GameObject.Find("Main Camera");
+
+        // VideoPlayer automatically targets the camera backplane when it is added
+        // to a camera object, no need to change videoPlayer.targetCamera.
+        videoPlayer = camera.AddComponent<UnityEngine.Video.VideoPlayer>();
+
+        // Play on awake defaults to true. Set it to false to avoid the url set
+        // below to auto-start playback since we're in Start().
+        videoPlayer.playOnAwake = false;
+
+        // By default, VideoPlayers added to a camera will use the far plane.
+        // Let's target the near plane instead.
+        videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.CameraNearPlane;
+
+        // Set the video to play. URL supports local absolute or relative paths.
+        // Here, using absolute.
+        videoPlayer.url = "/Users/Jaymu/Downloads/crash.mp4";
+
+        // Skip the first 100 frames.
+        //     videoPlayer.frame = 100;
+
+        // Restart from beginning when done.
+        videoPlayer.isLooping = true;
+
+        // Start playback. This means the VideoPlayer may have to prepare (reserve
+        // resources, pre-load a few frames, etc.). To better control the delays
+        // associated with this preparation one can use videoPlayer.Prepare() along with
+        // its prepareCompleted event.
+        videoPlayer.Prepare();
+
+
         isCrashed = false;
         _audio = GetComponent<AudioSource>();
         if (SceneManager.GetActiveScene().name.Equals("Level1"))
@@ -48,11 +79,12 @@ public class PlayerControl : MonoBehaviour
         // print(other.gameObject);
         if (other.gameObject.CompareTag("NPC"))
         {
+            videoPlayer.Play();
             Debug.Log("collision");
             _audio.PlayOneShot(carCrash, 100);
             isCrashed = true;
 
-            StartCoroutine(Crash());
+            //   StartCoroutine(Crash());
         }
 
         if (other.gameObject.CompareTag("END"))
